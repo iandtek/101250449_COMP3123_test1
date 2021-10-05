fee3d991eef5b011080220d672b71ae44213c80f
Simon Bermudez <sabdz55@gmail.com>
2021-10-05 14:34:33 -0700

    added sync and async version of removeLogFiles

--- question-3/removeLogFiles.js
+++ question-3/removeLogFiles.js
@@ -2,21 +2,40 @@ const fs = require('fs')
 const path = require('path')
 const logsDir = './logs'
 
-function removeLogFiles() {
+function removeLogFilesSync() {
     // Output the file names to delete
-    fs.readdirSync(logsDir).forEach(fileName => {
-        console.log(path.join(fileName))
-    })
-
     // Remove all the logs from the log directory
     // remove logs directory
-    
-    fs.rmdir(logsDir, { recursive: true }, (err) => {
-        if (err) {
-            throw err
-        }
-        console.log(`${logsDir} is deleted!`)
-    });
+
+    fs.readdirSync(logsDir).forEach(fileName => {
+        fs.unlinkSync(path.join(logsDir, fileName))
+        console.log(path.join(fileName))
+    })
+    fs.rmdirSync(logsDir)
+    console.log(`${logsDir} has been deleted.`)
+
 }
 
-removeLogFiles()
+function removeLogFilesAsync() {
+    fs.readdir(logsDir, (err, files) => {
+        if (err)
+          console.log(err)
+        else {
+          files.forEach(file => {
+            fs.unlink(path.join(logsDir, file), function (err) {
+                if (err) throw err
+                // if no error, file has been deleted successfully
+                console.log(file)
+            });
+          })
+        // Delete Directory
+        fs.rmdir(logsDir, function (err) {
+            if (err) throw err
+            // if no error, file has been deleted successfully
+            console.log(`${logsDir} has been deleted.`)
+        })
+        }
+      })
+}
+
+removeLogFilesSync()
